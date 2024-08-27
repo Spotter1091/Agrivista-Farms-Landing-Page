286f2db4fbf7163046c0687ce2ab092c45cebcac
Spotter1091 <adedayolamide.o@gmail.com>
2024-08-27 11:17:41 +0100

    update home file

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import farmland from '../../assets/image/farm-land.png';
+import "./Home.css";
 
 export const Home = () => {
-
-
   return (
     <div>
       <div className="hero-container">
@@ -18,9 +17,40 @@ export const Home = () => {
             existentie es un myth. Por scientie, musica, sport etc, litot Europa
             usa li sam vocabular.
           </p>
+          <div className="primary-btn">
+            <a href="#" className="get-strt-btn">
+              Get Started
+            </a>
+
+            <a href="#" className="learn-mre-btn">
+              Learn More
+            </a>
+          </div>
         </div>
       </div>
-      hbvnjfvjsnd
+
+      <section className="passion-sect">
+        <div className="text-section">
+          <h2>
+            Our Passion for Agriculture Nurturing Growth and Sustaining the
+            Future
+          </h2>
+          <p>
+            Li Europan lingues es membres del sam familie. Lor separat
+            existentie es un myth. Por scientie, musica, sport etc, litot Europa
+            usa li sam vocabular..
+          </p>
+          <a href="#" className="get-strt-btn">
+            Get Started
+          </a>
+        </div>
+        <div className="farmland-image">
+          <img src={farmland} alt="farm land" />
+          <a href="#" className="get-strt-btn">
+            Get Started
+          </a>
+        </div>
+      </section>
     </div>
   );
-}
+};
