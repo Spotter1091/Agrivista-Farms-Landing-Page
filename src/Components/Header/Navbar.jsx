258bd5ad29851f8f52f1f8e80ab9275c00427a03
Spotter1091 <adedayolamide.o@gmail.com>
2024-08-29 23:27:09 +0100

    update navbar file

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
-// import logo from '../../assets/image/logo1.png'
+import React, {useState} from 'react'
+// import logo from '../../assets/image/logo1.png';
+import { FaBars } from "react-icons/fa";
 import './Navbar.css'
 
 
+
 export const Navbar = () => {
+
+   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+   const toggleMobileMenu = () => {
+     setIsMobileMenuOpen(!isMobileMenuOpen);
+   };
+
+
+const style = { color: "black", fontSize: "2em" };
+
+
   return (
     <nav className="nav-container">
       <div className="logo-box">
         {/* <img src={logo} className='logo' alt="logo" /> */}
         <h1>Agrivista Farms </h1>
       </div>
-      <ul>
+      <ul className='nav-menu'>
         <li>Home</li>
         <li>About Us</li>
         <li>Pages</li>
-         <li><a href="#" className='btn-login'>Login</a></li> 
-         <li><a href="#" className='btn-signup'>Sign Up</a></li> 
-        
-        
+        <li>
+          
+          <a href="#" className="btn-login">
+            Login
+          </a>
+        </li>
+        <li>
+          <a href="#" className="btn-signup">
+            Sign Up
+          </a>
+        </li>
       </ul>
+      <div className="hamburger" onClick={toggleMobileMenu}>
+        <FaBars style={style} />
+      </div>
     </nav>
   );
 }
